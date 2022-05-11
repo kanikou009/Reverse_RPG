a42052eec9b455f6df4a7f05787a31494b36e599
Ryusei1115 <tekunikaru22@gmail.com>
2022-05-11 16:57:07 +0900

    初期設定

---
+++ Assets/Scripts/Data/EnemyData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "EnemyData", menuName = "CreateEnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public string Name => _name;
+    public int HP => _hp;
+    public int MP => _mp;
+    public int MaxHP => _maxHp;
+    public int MaxMP => _maxMp;
+    public int Power => _power;
+    public int DefensePower => _defensePower;
+    public int Speed => _speed;
+
+    [SerializeField]
+    [Header("Enemyの名前")]
+    string _name;
+
+    [SerializeField]
+    [Header("EnemyのHP")]
+    int _hp;
+
+    [SerializeField]
+    [Header("EnemyのMP")]
+    int _mp;
+
+    [SerializeField]
+    [Header("Enemyの攻撃力")]
+    int _power;
+
+    [SerializeField]
+    [Header("Enemyの防御力")]
+    int _defensePower;
+
+    [SerializeField]
+    [Header("Enemyの素早さ")]
+    int _speed;
+
+    [SerializeField]
+    [Header("Enemyの最大HP")]
+    int _maxHp;
+
+    [SerializeField]
+    [Header("Enemyの最大MP")]
+    int _maxMp;
+}
---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "PlayerData" , menuName = "CreatePlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public string Name => _name;
+    public int HP => _hp;
+    public int MP => _mp;
+    public int MaxHP => _maxHp;
+    public int MaxMP => _maxMp;
+    public int Power => _power;
+    public int DefensePower => _defensePower;
+    public int Speed => _speed;
+
+    [SerializeField]
+    [Header("Playerの名前")]
+    string _name;
+
+    [SerializeField]
+    [Header("PlayerのHP")]
+    int _hp;
+
+    [SerializeField]
+    [Header("PlayerのMP")]
+    int _mp;
+
+    [SerializeField]
+    [Header("Playerの攻撃力")]
+    int _power;
+
+    [SerializeField]
+    [Header("Playerの防御力")]
+    int _defensePower;
+
+    [SerializeField]
+    [Header("Playerの素早さ")]
+    int _speed;
+
+    [SerializeField]
+    [Header("Playerの最大HP")]
+    int _maxHp;
+
+    [SerializeField]
+    [Header("Playerの最大MP")]
+    int _maxMp;
+}

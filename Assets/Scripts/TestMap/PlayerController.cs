eb3370c927cee8ef9ae9badff55273c7e2f6d305
masa5956 <92581382+masa5956@users.noreply.github.com>
2022-05-23 02:17:54 +0900

    mapの作成、NPC、オブジェクトの設置、会話機能、様々なSpriteの追加

---
+++ Assets/Scripts/TestMap/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+
+
+
+
+
+    private float activeMoveSpeed;
+
+
+
+
+
+
+
+
+    [SerializeField, Tooltip("移動スピード")]
+    private int movespeed;
+
+    [SerializeField]
+    private Animator playerAnime;
+
+
+
+
+
+    public Rigidbody2D rb;
+
+    public static PlayerController instance;
+    // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+
+
+
+
+        //移動
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * activeMoveSpeed;
+
+
+        if (rb.velocity != Vector2.zero)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    playerAnime.SetFloat("X", 1f);
+                    playerAnime.SetFloat("Y", 0);
+
+
+                }
+                else
+                {
+                    playerAnime.SetFloat("X", -1f);
+                    playerAnime.SetFloat("Y", 0);
+
+                }
+
+            }
+            else if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                playerAnime.SetFloat("X", 0);
+                playerAnime.SetFloat("Y", 1);
+
+
+            }
+            else
+            {
+                playerAnime.SetFloat("X", 0);
+                playerAnime.SetFloat("Y", -1);
+
+
+            }
+        }
+    }
+}
+
+
+       
---
+++ Assets/Scripts/TestMap/testDialog.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testDialog : MonoBehaviour
+{
+    public Canvas CanvasS;
+
+    public GameObject dialogBox;
+    public Text dialogText;
+
+    private string[] dialogLines;
+
+    private int currentLine;
+    private bool justStarted;
+
+
+    
+
+    public static testDialog instance;
+
+    private void Awake()
+    {
+        if(instance== null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetMouseButtonUp(1))
+            {
+                if (!justStarted)
+                {
+                    currentLine++;
+
+                    if (currentLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+                        CanvasS.enabled = true;
+                    }
+                    else
+                    {
+                        dialogText.text = dialogLines[currentLine];
+                    }
+                }
+                else
+                {
+                    justStarted = false;
+                }
+            }
+        }
+    }
+
+  
+    public void ShowDialog(string[]lines)
+    {
+        dialogLines = lines;
+
+        currentLine = 0;
+
+        dialogText.text = dialogLines[currentLine];
+        dialogBox.SetActive(true);
+
+        justStarted = true;
+
+
+    }
+
+    public void ShowDialogChange(bool x)
+    {
+        dialogBox.SetActive(x);
+        CanvasS.enabled = !x;
+    }
+}

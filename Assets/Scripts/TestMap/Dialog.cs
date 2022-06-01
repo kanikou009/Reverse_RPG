ed9f87853d91a9c09f1d9f30258152b7a01403b2
masa5956 <92581382+masa5956@users.noreply.github.com>
2022-06-01 15:09:39 +0900

    会話システムスクリプト作成

---
+++ Assets/Scripts/TestMap/Dialog.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[RequireComponent(typeof(Flowchart))]
+public class Dialog : MonoBehaviour
+{
+
+    [SerializeField]
+    string messsage = "";
+
+    public bool canActivater;
+
+    GameObject playerObject;
+
+
+
+   
+    Flowchart flowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        flowchart = GetComponent<Flowchart>();
+        
+
+
+    } 
+    void Update()
+        {
+            if (Input.GetMouseButtonDown(1)&& canActivater)
+            {
+                StartCoroutine(Talk());
+
+           
+            }
+
+
+            IEnumerator Talk()
+        {
+
+          
+
+            flowchart.SendFungusMessage(messsage);
+            yield return new WaitUntil(() => flowchart.GetExecutingBlocks().Count == 0);
+
+          
+        }
+        }
+    private void OnCollisionEnter2D(UnityEngine.Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canActivater = true;
+
+           
+        }
+        else
+        {
+           
+        }
+
+    }
+    private void OnCollisionExit2D(UnityEngine.Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+
+        {
+            canActivater = false;
+
+          
+
+        }  //gameObject.GetComponent<PlayerController>().enabled = false; //gameObject.GetComponent<PlayerController>().enabled = true;
+
+
+       
+
+        // Update is called once per frame
+      
+
+    }
+}
+

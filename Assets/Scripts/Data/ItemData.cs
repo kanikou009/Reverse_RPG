8d6cf5690b73f9a2c3cacb9c36934f59ce064117
Ryusei1115 <tekunikaru22@gmail.com>
2022-06-01 15:18:41 +0900

    ItemのDataを作成

---
+++ Assets/Scripts/Data/ItemData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "ItemData", menuName = "CreateItemData")]
+public class ItemData : ScriptableObject
+{
+    public Param Parameter => _param;
+    public int Heel => _heel;
+    public string Name => _name;
+    public string Explanation => _explanation;
+    public Sprite ItemSprite => _sprite;
+
+    [SerializeField]
+    [Header("回復するパラメータ")]
+    Param _param = Param.HP;
+
+    [SerializeField]
+    [Header("回復量")]
+    int _heel;
+
+    [SerializeField]
+    [Header("アイテム名")]
+    string _name;
+
+    [SerializeField]
+    [Header("説明")]
+    string _explanation;
+
+    [SerializeField]
+    [Header("画像")]
+    Sprite _sprite;
+
+    public enum Param
+    {
+        HP,
+        MP
+    }
+}

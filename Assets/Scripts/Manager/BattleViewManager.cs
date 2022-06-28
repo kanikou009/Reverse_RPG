fe10b71f7e4677834e00d5731af50c990130799b
Ryusei1115 <tekunikaru22@gmail.com>
2022-06-28 21:45:21 +0900

    WIP

--- Assets/Scripts/Manager/BattleViewManager.cs
+++ Assets/Scripts/Manager/BattleViewManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
-using System;
 
 public class BattleViewManager : SingletonMonoBehaviour<BattleViewManager>
 {
